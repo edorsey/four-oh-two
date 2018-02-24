0c1c0e646e2bea73583a0d8637c00ccd68ae37c9
Eric Dorsey <eric@ericdorsey.com>
2018-02-24 16:30:43 -0600

    Add service wallet to account list

--- src/payment-manager/config/nano-accounts.js
+++ src/payment-manager/config/nano-accounts.js
@@ -1,6 +1,7 @@
 let nanoAccounts = [
   "xrb_1niabkx3gbxit5j5yyqcpas71dkffggbr6zpd3heui8rpoocm5xqbdwq44oh", //KUCoin Exchange
   "***REMOVED***", //My iOS Wallet
+  "xrb_3b3cfcn95mb96f1xfpjyj8cz5wkdhgmwh837xk94ndej9y46uyntkwckouni", //Service Wallet
 ]
 
 module.exports = nanoAccounts
