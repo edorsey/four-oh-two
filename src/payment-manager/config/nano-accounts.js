12a5af80a373237f733694cdef9b160cf1883ab4
Eric Dorsey <eric@ericdorsey.com>
2018-02-24 14:34:52 -0600

    get bootstrapping working

---
+++ src/payment-manager/config/nano-accounts.js
@@ -0,0 +1,6 @@
+let nanoAccounts = [
+  "xrb_1niabkx3gbxit5j5yyqcpas71dkffggbr6zpd3heui8rpoocm5xqbdwq44oh", //KUCoin Exchange
+  "***REMOVED***", //My iOS Wallet
+]
+
+module.exports = nanoAccounts
--- src/payment-manager/services/nano/bootstrap-node-data.js
+++ src/payment-manager/services/nano/bootstrap-node-data.js
@@ -1,6 +1,36 @@
+let async = require("async")
+let NanoClient = require("node-raiblocks-rpc")
+
+let nanoAccounts = require("../../config/nano-accounts")
+
+const NANO_NODE_ADDRESS = "http://192.168.1.5:7076"
+const nano = new NanoClient(NANO_NODE_ADDRESS, true)
+
 function bootstrap(done) {
   console.log("BOOTSTRAP")
-  done()
+  async.series([
+    wait,
+    (next) => async.mapSeries(nanoAccounts, getAccountInfo, next)
+  ], done)
 }
+
+function getAccountInfo(account, cb) {
+  console.log("HERE", account)
+  nano.account_info(account).then(function(accountInfo) {
+    console.log("ACCOUNT INFO", accountInfo)
+    cb()
+  }).catch(cb)
+}
+
+
+function wait(next) {
+  setTimeout(function() {
+    next()
+  }, 1000)
+}
+
+bootstrap(function(err) {
+  console.log("boostraped")
+})
 
 module.exports = bootstrap
