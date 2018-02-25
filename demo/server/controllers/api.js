16d7666c572e11d8dc41d2c1ec0ba1e5eb319550
Eric Dorsey <eric@ericdorsey.com>
2018-02-25 17:35:43 -0600

    get it working

--- demo/server/controllers/api.js
+++ demo/server/controllers/api.js
@@ -2,12 +2,15 @@ let express = require("express")
 let router = express.Router()
 let fourOhTwo = require("../../../src/server-module")
 
+const NANO_ACCOUNT = "xrb_3b3cfcn95mb96f1xfpjyj8cz5wkdhgmwh837xk94ndej9y46uyntkwckouni"
+
 router.use(fourOhTwo({
-  cost: .0001,
+  cost: "100000000000000000000000000",
   costPer: "access",
   currency: "NANO",
-  serviceAccount: process.env.NANO_ACCOUNT,
-  hostname: process.env.HOSTNAME
+  serviceAccount: NANO_ACCOUNT,
+  hostname: process.env.HOSTNAME,
+  paymentManagerURI: "http://localhost:3402"
 }))
 
 router.get("/", function(req, res, next) {
